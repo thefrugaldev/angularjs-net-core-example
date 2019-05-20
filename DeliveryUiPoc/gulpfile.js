cc0b9af09cc960866fde5749eb27b4063aba27cc
Orman, Clayton <CDO8081@yum.com>
2019-05-20 14:37:00 -0400

    Update gulpfile to clean and copy files

--- DeliveryUiPoc/gulpfile.js
+++ DeliveryUiPoc/gulpfile.js
@@ -1,29 +1,19 @@
 ﻿/// <binding AfterBuild='build-all' />
-
 var gulp = require("gulp");
 
 var root_path = {
-    webroot: "./wwwroot/"
+    webroot: "./wwwroot/",
+    app_src: "./src/",
+    package_lib: root_path.webroot + "app"
 };
 
-//library source
-root_path.nmSrc = "./bower_components/";
-
-//library destination
-root_path.package_lib = root_path.webroot + "lib/";
-
-
-gulp.task('copy-lib-css', function () {
-    gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
-        .pipe(gulp.dest(root_path.webroot + '/css/'));
+gulp.task('clean', function () {
+    return del([root_path.package_lib]);
 });
 
-gulp.task('copy-lib-js', function () {
-    gulp.src('./bower_components/angular/angular.min.js')
-        .pipe(gulp.dest(root_path.package_lib + 'angular/'));
-    gulp.src('./bower_components/angular-ui-router/release/angular-ui-router.min.js')
-        .pipe(gulp.dest(root_path.package_lib + 'angular/'));
+gulp.task('copy-src', ['clean'], function () {
+    gulp.src(root_path.app_src)
+        .pipe(gulp.dest(root_path.package_lib));
 });
 
-gulp.task("copy-all", ["copy-lib-css", "copy-lib-js"]);
-//Copy End
+gulp.task("copy-all", ["clean", "copy-src"]);
